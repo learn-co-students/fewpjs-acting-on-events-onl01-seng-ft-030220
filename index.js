cece565ea590e9b9c1052af84d956fe0250a78a0
marsha <marshaannon@gmail.com>
2020-06-09 14:46:57 -0400

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight(){
     let leftNumbers = dodger.style.left.replace("px", "")
     let left = parseInt(leftNumbers, 10)
 
-    if (left > 0 && left < 400) {
+    if (left > 0 && left < 360) {
         dodger.style.left = `${left +1}px`
     }
 }
